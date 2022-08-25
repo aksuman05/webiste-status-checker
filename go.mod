8c87e15388b4936420dc268e5e551cb8ea98ca79
Abhishek Suman <abhishek.suman@swiggy.in>
2022-08-26 01:55:19 +0530

    adding project to github

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aksuman05/website-status-checker
+
+go 1.13
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/aksuman05/website-status-checker/server"
+
+func main() {
+	server.Start()
+}
